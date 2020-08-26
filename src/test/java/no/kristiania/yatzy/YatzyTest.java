fe09b998dc610ebefcb6e6e1e7b7fd45ee18b7e3
michael <michaelkeenan@protonmail.com>
2020-08-26 16:46:26 +0200

    commited another failed test

--- src/test/java/no/kristiania/yatzy/YatzyTest.java
+++ src/test/java/no/kristiania/yatzy/YatzyTest.java
@@ -6,8 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class YatzyTest {
 
+    private YatzyGame yatzyGame = new YatzyGame();
+
     @Test
     void shouldScoreOnes(){
-        assertEquals(4, new YatzyGame().score(YatzyCategory.ONES, new int[] {1, 1, 2, 1, 1}));
+        assertEquals(4, yatzyGame.score(YatzyCategory.ONES, new int[] {1, 1, 2, 1, 1}));
+        assertEquals(2, yatzyGame.score(YatzyCategory.ONES, new int[] {6, 5, 2, 1, 1}));
+
     }
 }
